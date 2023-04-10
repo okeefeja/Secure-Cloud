5a64bbcef1493d8700246289baf466e60750cf35
okeefeja <98922393+okeefeja@users.noreply.github.com>
2023-04-10 20:26:49 +0100

    Create user.py

---
+++ user.py
@@ -0,0 +1,64 @@
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509.oid import NameOID
+from datetime import datetime, timedelta
+from cryptography.hazmat.primitives import serialization
+
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.setUserKeys()
+
+    def setUserKeys(self):
+        # Generate a new RSA key pair
+        key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+        )
+
+        # Create a new self-signed certificate
+        subject = issuer = x509.Name([
+            x509.NameAttribute(NameOID.COMMON_NAME, self.name),
+            x509.NameAttribute(NameOID.EMAIL_ADDRESS, self.email),
+        ])
+        now = datetime.utcnow()
+        self.certificate = x509.CertificateBuilder().subject_name(
+            subject
+        ).issuer_name(
+            issuer
+        ).public_key(
+            key.public_key()
+        ).serial_number(
+            x509.random_serial_number()
+        ).not_valid_before(
+            now
+        ).not_valid_after(
+            now + timedelta(days=365)
+        ).add_extension(
+            x509.SubjectAlternativeName([x509.DNSName('example.com')]),
+            critical=False,
+        ).sign(key, hashes.SHA256())
+
+        # Extract the private key and public key as variables
+        private_key_bytes = key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+
+        private_key = serialization.load_pem_private_key(
+            private_key_bytes,
+            password=None
+        )
+
+        # Write the email and private key to a local text file as a tuple
+        with open('keys.txt', 'a') as f:
+
+            # Might have to fix as will probably add multiple private keys
+            # file_contents = f.read()
+            # if len(file_contents) > 0:
+            #     f.truncate(0)
+            
+            f.write(f'({self.email}, {private_key_bytes.decode("utf-8")}),\n')
