150854f7b34716939993f1e13cedcd685fff3b5a
okeefeja <98922393+okeefeja@users.noreply.github.com>
2023-04-10 20:42:47 +0100

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,7 +2,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
 from user import User
-from file import uploadFile
+from file import uploadFile, encryptFile
 
 newUser = User("James O'Keefe", "jcokeefe02@gmail.com")
 
@@ -16,24 +16,4 @@ public_key = serialization.load_pem_public_key(
     public_key_pem
 )
 
-# Open the file to be encrypted
-with open('file.txt', 'rb') as f:
-    plaintext = f.read()
-
-# Encrypt the file using the RSA public key
-ciphertext = public_key.encrypt(
-    plaintext,
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
-
-# Save the encrypted file to disk
-with open('cipherFile.txt', 'wb') as f:
-    f.write(ciphertext)
-
-uploadFile('cipherFile.txt')
-
-print("Successfully uploaded the encryption of 'file.txt' to Box")
+encryptFile('file.txt', public_key)
